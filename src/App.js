d5bb8d63290abdf46d26297d8a8ef6ad938c0134
Dima Dediukhin <dde6156@gmail.com>
2024-11-26 22:30:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+
+import GoodsCard from './components/GoodsCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoodsCard src="../images/apple.png" title="Apple" cost={15}/>
+      <GoodsCard src="../images/cherry.png" title="Cherry" cost={30}/>
+      <GoodsCard src="../images/strawberry.png" title="Strawberry" cost={35}/>
     </div>
   );
 }
---
+++ src/components/GoodsCard.js
@@ -0,0 +1,12 @@
+function GoodsCard(props) {
+    return(
+        <div className="card">
+            <img className="fruitImg" src={props.src} alt="Fruit Image" height="260" width="260"/>
+            <p className="fruitTitle">{props.title}</p>
+            <p className="fruitCost">Cost: {props.cost}</p>
+        </div>
+        
+    );
+}
+
+export default GoodsCard;
