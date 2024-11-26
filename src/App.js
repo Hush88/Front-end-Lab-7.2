e3b941b6a09c9269f1528a52ab8d4e2f13c74e99
Dima Dediukhin <dde6156@gmail.com>
2024-11-26 22:51:04 +0200

    3 commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import GoodsCard from './components/GoodsCard';
 function App() {
   return (
     <div className="App">
-      <GoodsCard src="./images/apple.png" title="Apple" cost={15}/>
+      <GoodsCard src="/images/apple.png" title="Apple" cost={15}/>
       <GoodsCard src="./images/cherry.png" title="Cherry" cost={30}/>
-      <GoodsCard src="./images/strawberry.png" title="Strawberry" cost={35}/>
+      <GoodsCard src="../images/strawberry.png" title="Strawberry" cost={35}/>
     </div>
   );
 }
--- src/components/GoodsCard.js
+++ src/components/GoodsCard.js
@@ -1,7 +1,7 @@
 function GoodsCard(props) {
     return(
         <div className="card">
-            <img className="fruitImg" src={props.src} alt="Fruit Image" height="260" width="260"/>
+            <img className="fruitImg" src={props.src} alt="Fruit" height="260" width="260"/>
             <p className="fruitTitle">{props.title}</p>
             <p className="fruitCost">Cost: {props.cost}</p>
         </div>
